5a77d9229ec324c1e8206fee506954f2060af8d9
Memi3 <86162536+Memi3@users.noreply.github.com>
2022-04-04 01:56:29 -0600

    cambiar contra2

--- Client/js/CambiarContra.js
+++ Client/js/CambiarContra.js
@@ -4,8 +4,16 @@
 async function NuevaContra() {
 
     const url = "https://proyectofinalsw.azurewebsites.net/api/"
+    let Id = localStorage.getItem("editID")
+    let Username = localStorage.getItem("editUser")
+
     const npasswordDOM = document.getElementById("npassword")
     const password2DOM = document.getElementById("password2")
+
+    let Email = localStorage.getItem("editEmail")
+    let Role = localStorage.getItem("editRole")
+
+
     const objeto = {
         password: passwordDOM.value,
         npassword: npasswordDOM.value
@@ -27,23 +35,24 @@ function VerificarClave() {
         document.getElementById("warning").innerHTML = "Contraseña debe ser igual a la ingresada en el campo anterior"
     }
 }
-
 function contrasenaOnchageEvento() {
     const npasswordDOM = document.querySelector("#npasswordtxt")
     const password2DOM = document.getElementById("password2txt")
     console.log(npasswordDOM)
+    console.log(password2DOM)
     npasswordDOM.addEventListener("change", (e) => {
-        localStorage.setItem("contrasena1", e.value)
+        localStorage.setItem("contrasena1", e.target.value)
         const contrasena1local = localStorage.getItem("contrasena1")
         console.log(contrasena1local)
-
     })
-
-
-
-
-
+    password2DOM.addEventListener("change", (e) => {
+        localStorage.setItem("contrasena2", e.target.value)
+        const contrasena2local = localStorage.getItem("contrasena2")
+        console.log(contrasena2local)
+    })
 }
+
+
 function main(){
  
     contrasenaOnchageEvento()
